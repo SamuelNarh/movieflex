b09ab58209c263c62226f65c9e61e3b556feb5ea
[Samuel Narh] <samuelnarh38@gmail.com>
2025-08-04 08:52:02 +0000

    added appwrite database to movieflex to fetch trending movies from the database

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { Search } from './components/Search'
 import { Loader } from './components/Loader';
 import { MovieCard } from './components/MovieCard';
+import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
-// const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDQzZWM5ODFjZmFmNzY1OWUwY2I3MzhjOTU5NWRhMiIsIm5iZiI6MTc1NDE2ODA2My4xODIwMDAyLCJzdWIiOiI2ODhlN2FmZjgyNWRkN2RkMTJhNDYyMGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Qnxbla5-7GdBETEiWvQooTyopkGSsnWzooFYTXkHYfM"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
   method: 'GET',
   headers: {
@@ -17,26 +18,34 @@ const API_OPTIONS = {
 export default function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessasage, seterrorMessasage] = useState('');
-  const [movieList, setMovieList] = useState([])
-  const [isLoading, setisLoading] = useState(false)
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [debouncedSearchTerm, setdebouncedSearchTerm] = useState('');
+  const [trendingMovies, settrendingMovies] = useState([]);
 
-  const fetchMovies = async (query ='') => {
+  // Debounce the search request to prevent making too many API request
+  useDebounce(() => setdebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
     setisLoading(true)
     seterrorMessasage('')
     try {
-      const endpoint = query?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`:`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS)
       if (!response.ok) {
         throw new Error("Failed to fetch Movies");
       }
       const data = await response.json()
-      console.log(data)
       if (data.Response === 'False') {
         seterrorMessasage(data.Error || 'failed to fetch movies !!!')
         setMovieList([])
         return;
       }
-      setMovieList(data.results || [])
+      setMovieList(data.results || []);
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
 
     }
     catch (error) {
@@ -47,10 +56,29 @@ export default function App() {
       setisLoading(false);
     }
   };
+
+  const LoadTrendingMovies = async () => {
+
+    try {
+
+      const movies = await getTrendingMovies();
+      settrendingMovies(movies)
+      console.log(movies)
+    } catch (error) {
+      console.error(`Error fetching Trending Movies: ${error}`);
+    }
+  }
+
+
   useEffect(() => {
-    fetchMovies(searchTerm);
+    fetchMovies(debouncedSearchTerm);
   },
-    [searchTerm])
+    [debouncedSearchTerm])
+
+  useEffect(() => {
+    LoadTrendingMovies()
+  }, [])
+
 
   return (
     <main>
@@ -62,8 +90,28 @@ export default function App() {
           <h1>Find <span className='text-gradient' >Movies</span>You'll Enjoy Without the Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+        {
+          trendingMovies.length > 0 &&(
+            <section className='trending'>
+              <h2>
+                Trending Movies
+              </h2>
+              <ul>
+                {
+                  trendingMovies.map((trending, index)=>(
+                    <li key={trending.$id}>
+                      <p>{index+1}</p>
+                      <img src={trending.poster_url} alt={trending.title} />
+                    </li>
+                  ))
+                }
+              </ul>
+
+            </section>
+          )
+        }
         <section className='all-movies'>
-          <h2 className='mt-[40px]'>All Movies</h2>
+          <h2>All Movies</h2>
           {
             isLoading ? (<Loader />) : errorMessasage ? (<p className='text-red-500'>{errorMessasage}</p>) :
               (
